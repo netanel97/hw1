3528758e90a4eed574cd2eb21fd810879aae5ca1
netanel97 <netanelhabas@gmail.com>
2024-06-10 08:22:43 +0300

    Fixing the problem with the share preference

--- common/src/main/java/com/example/common/HomeFragment.java
+++ common/src/main/java/com/example/common/HomeFragment.java
@@ -17,7 +17,9 @@ import android.widget.Toast;
 
 import com.example.common.databinding.FragmentMainBinding;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 
@@ -27,24 +29,24 @@ public class HomeFragment extends Fragment {
 
     private HomeRecyclerViewAdapter mAdapter;
     private RecyclerView resultRV;
-    private ArrayList<Note> notesList;
 
     private SharedPreferences sp;
 
-    private static final String PREFS_NAME = "ReHubPrefs";
+    private static final String PREFS_NAME = "TodoListPrefs";
 
+    private DataManager dataManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        notesList = new ArrayList<>(); // Initialize notes list
         sp = getContext().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-
+        dataManager = new DataManager();
+        loadPreferences();
         binding = FragmentMainBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
         resultRV = binding.homeFragmentLSTTextList; // Connect between xml to Recyclerview
         mAdapter = new HomeRecyclerViewAdapter(getContext());
-//        mAdapter.updateList(); TODO:need to send data or pull data from the db need to think
+        mAdapter.updateList(this.dataManager.getAllNotes());
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
         resultRV.setAdapter(mAdapter);
@@ -57,14 +59,31 @@ public class HomeFragment extends Fragment {
         });
         return view;
     }
+
+    private void loadPreferences() {
+        String notesJson = sp.getString(PREFS_NAME, null);
+
+        // Check if there are any notes saved
+        if (notesJson != null) {
+            // Deserialize the JSON string into an ArrayList of Notes
+            Gson gson = new Gson();
+            Type type = new TypeToken<ArrayList<Note>>() {
+            }.getType();
+            ArrayList<Note> allNotes = gson.fromJson(notesJson, type);
+
+            // Set the loaded notes to your DataManager
+            dataManager.setAllNotes(allNotes);
+        }
+    }
+
     private void savePreferences() {
         // Serialize the ArrayList of Notes into a JSON string
         Gson gson = new Gson();
-        String notesJson = gson.toJson(notesList);
+        String notesJson = gson.toJson(dataManager.getAllNotes());
 
         // Save the JSON string to SharedPreferences
         SharedPreferences.Editor editor = sp.edit();
-        editor.putString("notes", notesJson);
+        editor.putString(PREFS_NAME, notesJson);
         editor.apply();
     }
 
@@ -77,10 +96,10 @@ public class HomeFragment extends Fragment {
 
         builder.setPositiveButton("Add", (dialog, which) -> {
             String note = input.getText().toString();
-            Note newNote = new Note(note,false);
+            Note newNote = new Note(note, false);
             if (!note.isEmpty()) {
-                notesList.add(newNote);
-                mAdapter.updateList(notesList);
+                dataManager.getAllNotes().add(newNote);
+                mAdapter.updateList(dataManager.getAllNotes());
                 savePreferences();
 
             } else {
